c6b487a65703b66745890f9b8ff3733cf8a63d21
DRIVE AREA Four <drive.area.four@gmail.com>
2026-02-15 21:15:09 +0600

    code

---
+++ AdmissionTest/AdmissionTest/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Task1;
+
+//Console.WriteLine("Hello World");
+
+namespace GetDoubleValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Age = { 21, 25, 30, 35, 40 };
+            //int result = Age.Calculate(Age, x => x*2);
+            int result = Age.Calculate(x => x * 2);
+            int old = Age[Age.Length / 2];
+            Console.WriteLine("Age was:" + old + "And Returned is:" + result);
+        }
+    }
+}
---
+++ AdmissionTest/AdmissionTest/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bulb bulb = new Bulb();
+            bulb.BulbSituation += Print;
+
+            bulb.Off();
+
+
+        }
+        static void Print(bool situation)
+        {
+            if(situation == true)
+            {
+                Console.WriteLine("Light is ON");
+            }
+            else
+            {
+                Console.WriteLine("Light is Off");
+            }
+        }
+    }
+}
