91e17dd7b60d4ae1f9ba38646e49b44e432e4222
DRIVE AREA Four <drive.area.four@gmail.com>
2026-02-15 21:29:26 +0600

    this is code

---
+++ AdmissionTest/Task2/Bulb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    public class Bulb
+    {
+        public bool Situation { get; set; }
+
+        public event Action<bool> BulbSituationON;
+        public event Action<bool> BulbSituationOFF;
+        public void On()
+        {
+            Situation = true;
+            if (BulbSituationON != null)
+            {
+                BulbSituationON(Situation);
+            }
+        }
+        public void Off()
+        {
+            Situation = false;
+            if (BulbSituationOFF != null)
+            {
+                BulbSituationOFF(Situation);
+            }
+        }
+
+
+    }
+}
---
+++ AdmissionTest/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bulb bulb = new Bulb();
+            bulb.BulbSituationON += Print;
+            bulb.BulbSituationOFF += Print;
+
+            bulb.Off();
+            bulb.On();
+
+        }
+
+        
+
+        static void Print(bool situation)
+        {
+            if(situation == true)
+            {
+                Console.WriteLine("Light is ON");
+            }
+            else
+            {
+                Console.WriteLine("Light is Off");
+            }
+        }
+    }
+}
